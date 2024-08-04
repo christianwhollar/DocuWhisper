6ad32d3d5235c6d10ad8aca0e29e07033068d27d
christianwhollar <christian.hollar@duke.edu>
2024-08-04 17:55:15 -0400

    add chunk saving

--- backend/src/embeddings.py
+++ backend/src/embeddings.py
@@ -15,8 +15,11 @@ class Embeddings:
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.model.to(self.device)
  
-    def get_embeddings(self, titles: List[str], texts: List[str], embedding_directory: str) -> Tuple[List[np.ndarray], List[str]]:
+    def get_embeddings(self, titles: List[str], texts: List[str], embedding_directory: str, chunk_directory:str = "chunks") -> Tuple[List[np.ndarray], List[str]]:
         os.makedirs(embedding_directory, exist_ok=True)
+        chunk_directory = f"{embedding_directory}/../{chunk_directory}"
+        os.makedirs(chunk_directory, exist_ok=True)
+        
         embeddings = []
         chunked_texts_with_titles = []
 
@@ -29,10 +32,16 @@ class Embeddings:
                 chunked_text_with_title = f"{title}_Chunk_{idx + 1}: {chunk}"
                 chunked_texts_with_titles.append(chunked_text_with_title)
 
-                file_path = os.path.join(embedding_directory, chunk_title + '.npy')
+                chunk_file_path = os.path.join(chunk_directory, chunk_title + '.txt')
 
-                if os.path.exists(file_path):
-                    embedding = np.load(file_path, allow_pickle=True).tolist()
+                if not os.path.exists(chunk_file_path):
+                    with open(chunk_file_path, 'w', encoding='utf-8') as chunk_file:
+                        chunk_file.write(chunked_text_with_title)
+
+                embedding_file_path = os.path.join(embedding_directory, chunk_title + '.npy')
+
+                if os.path.exists(embedding_file_path):
+                    embedding = np.load(embedding_file_path, allow_pickle=True).tolist()
                     embeddings.extend(embedding)
                 else:
                     inputs = self.tokenizer(chunk, return_tensors='pt', padding=True, truncation=True, max_length=512)
@@ -44,7 +53,7 @@ class Embeddings:
                     embedding = outputs.last_hidden_state.mean(dim=1).cpu().numpy()[0]
                     embeddings.append(embedding.astype(np.float32))
 
-                    np.save(file_path, [embedding])
+                    np.save(embedding_file_path, [embedding])
 
         return embeddings, chunked_texts_with_titles
 
