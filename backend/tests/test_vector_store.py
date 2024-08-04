2e2b7b1a4b3beee8b68fc75c91b9c54fed5f57a4
christianwhollar <christian.hollar@duke.edu>
2024-08-04 17:41:26 -0400

    fix vector store test for new embedding chunk method

--- backend/tests/test_vector_store.py
+++ backend/tests/test_vector_store.py
@@ -2,17 +2,17 @@
 from src.vector_store import VectorStore
 
 def test_vector_store(get_embeddings):
-    documents, embeddings, document_embeddings = get_embeddings
+    embeddings, document_embeddings, chunked_texts_with_titles  = get_embeddings
 
     embedding_dimension = len(document_embeddings[0])
     vector_store = VectorStore(dimension=embedding_dimension)
-    vector_store.add_documents(documents, document_embeddings)
+    vector_store.add_documents(chunked_texts_with_titles, document_embeddings)
 
     query = "This is the first document."
     k = 3
     query_embedding = embeddings.get_embeddings_query([query])[0]
     results = vector_store.search(query_embedding, k)
 
-    assert vector_store.documents == documents
+    assert vector_store.documents == chunked_texts_with_titles
     assert len(results) == k
-    assert documents[0] in results
+    assert chunked_texts_with_titles[0] in results
