2f4fca24cb001f2b8c634d26ae2ba93eac3c907f
christianwhollar <christian.hollar@duke.edu>
2024-08-04 22:18:30 -0400

    test commit

--- backend/tests/utils/mock_psycogp2.py
+++ backend/tests/utils/mock_psycogp2.py
@@ -6,3 +6,4 @@ mock_psycopg2 = MagicMock()
 sys.modules['psycopg2'] = mock_psycopg2
 sys.modules['psycopg2.extras'] = mock_psycopg2.extras
 sys.modules['psycopg2.extensions'] = mock_psycopg2.extensions
+
