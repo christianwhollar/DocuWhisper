3d5f2f2c2f0614169f383909dc9e295b51d56da2
christianwhollar <christian.hollar@duke.edu>
2024-08-05 23:36:47 -0400

    update docker image for token login

--- frontend/utils.py
+++ frontend/utils.py
@@ -2,6 +2,14 @@ import re
 
 
 def clean_response(response: str) -> str:
+    """_summary_
+
+    Args:
+        response (str): _description_
+
+    Returns:
+        str: _description_
+    """
     # Remove specific unwanted tags
     response = response.replace("</s>", "")
 
